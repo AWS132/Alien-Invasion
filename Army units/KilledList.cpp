bad7ba67b37261a540c217402c003399cbf2073c
Ahmed Waleed <ahmedwaleedshora123@gmail.com>
2024-04-13 20:53:37 +0200

    ADDED KILLED LIST

---
+++ Army units/KilledList.cpp
@@ -0,0 +1,38 @@
+#include "KilledList.h"
+
+KilledList::KilledList()
+{
+    count = 0;
+}
+
+bool KilledList::addUnit(ArmyUnit* Unit)
+{
+    count++;
+    return enqueue(Unit);
+}
+
+int KilledList::getCount() const
+{
+    return count;
+}
+
+void KilledList::printKilled()
+{
+   Node< ArmyUnit*>* temp= frontPtr;
+   cout << "============== Killed/Destructed Units ===============\n";
+   cout << count << "units [";
+   while (temp) {
+       temp->getItem()->Print();
+       if (!(temp->getNext()))cout << "]\n";
+       else cout << ", ";
+       temp = temp->getNext();
+   }
+}
+
+KilledList::~KilledList()
+{
+    ArmyUnit* x=nullptr;
+    while (dequeue(x)) {
+        if (x) delete x;
+    }
+}
---
+++ Army units/KilledList.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+#include "ArmyUnit.h"
+#include "../Data Structures/LinkedQueue.h"
+
+class KilledList:LinkedQueue<ArmyUnit*> {
+private:
+	int count;
+public:
+
+};
